460918f009ed397dbe9d75a99b66b37085c863a3
Job <jobengelen@yahoo.nl>
2021-12-28 07:47:36 +0100

    HelpMijnWasIsNat.nl
    Added homepage
    Added placeholder products
    Changed navbar

--- app/views/header.php
+++ app/views/header.php
@@ -1,34 +1,74 @@
 <!DOCTYPE html>
 <html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>MVC demo</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
-</head>
-<body>
- 
-<nav class="navbar navbar-expand-md navbar-dark bg-dark mb-4">
-  <div class="container">
-    <a class="navbar-brand" href="/">MVC demo</a>
-    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
-      <span class="navbar-toggler-icon"></span>
-    </button>
-    <div class="collapse navbar-collapse" id="navbarCollapse">
-      <ul class="navbar-nav me-auto mb-2 mb-md-0">
-        <li class="nav-item">
-          <a class="nav-link" href="/">Home</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="/article">Articles</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="/home/about">About</a>
-        </li>
-      </ul>     
-    </div>
-  </div>
-</nav>
 
-<div class="container">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Help! Mijn was is nat!</title>
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+  <!-- Bootstrap Font Icon CSS -->
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+</head>
+
+<body>
+  <nav class="navbar navbar-expand-md navbar-dark bg-dark mb-4">
+    <div class="container">
+      <a class="navbar-brand" href="/">HelpMijnWasIsNat.nl</a>
+      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarCollapse">
+        <ul class="navbar-nav me-auto mb-2 mb-md-0 w-25">
+          <li class="nav-item">
+            <a class="nav-link" href="/">Home</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="/article">Contact</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="/home/about">About</a>
+          </li>
+        </ul>
+        <ul class="navbar-nav me-auto mb-2 mb-md-0 w-50">
+          <li class="nav-item">
+            <span class="input-group-btn">
+              <div class="row justify-content-center">
+                <div class="card-body align-items-center">
+                  <div class="input-group">
+                    <input class="form-control form-control-lg form-control-borderless" type="search" placeholder="Zoek wasdrogers...">
+                    <button type="button" class="btn btn-secondary">
+                      <span class="glyphicon glyphicon-search"></span> Zoek
+                    </button>
+                  </div>
+                </div>
+              </div>
+            </span>
+          </li>
+        </ul>
+        <!-- right side of navbar -->
+        <ul class="navbar-nav me-auto mb-2 mb-md-0 w-25">
+          <li class="nav-item mx-5">
+            <div class="row justify-content-center">
+              <div class="card-body row no-gutters align-items-center">
+                <div class="input-group">
+                  <button type="button" class="btn btn-secondary">Registreer</button>
+                </div>
+              </div>
+            </div>
+          </li>
+          <li class="nav-item me-5">
+            <div class="row justify-content-center">
+              <div class="card-body row no-gutters align-items-center">
+                <div class="input-group">
+                  <button type="button" class="btn btn-light">Log in</button>
+                </div>
+              </div>
+            </div>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </nav>
+
+  <div class="container">
--- app/views/home/index.php
+++ app/views/home/index.php
@@ -1,6 +1,81 @@
 <?php
 include __DIR__ . '/../header.php';
 
-echo "<h1>Homepage!</h1>";
+?>
 
-include __DIR__ . '/../footer.php';
+<head>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css" />
+    <script src="https://kit.fontawesome.com/8a30a83fae.js" crossorigin="anonymous"></script>
+</head>
+<div class="row">
+    <div class="col-md-12">
+        <h1>HelpMijnWasIsNat.nl</h1>
+    </div>
+    <div class="col-md-2">
+        <p>filters</p>
+    </div>
+    <div class="col-md-9">
+        <?php
+
+        //$row = placeholder
+        $rows = array("1", "2", "3", "4", "5", "6", "7", "8", "9");
+
+        //Columns must be a factor of 12 (1,2,3,4,6,12)
+        $numOfCols = 3;
+        $rowCount = 0;
+
+        $bootstrapColWidth = 12 / $numOfCols;
+        foreach ($rows as $row) {
+            $rating = 3.5;
+
+            if ($rowCount % $numOfCols == 0) { ?> <div class="row">
+                <?php
+            }
+            $rowCount++;
+                ?>
+                <div class="col-md-<?php echo $bootstrapColWidth; ?> border pt-3 pb-3 ml-1 mr-1 mb-2">
+                    <div class="thumbnail">
+                        <img src="https://images.ctfassets.net/gtq5kcq8pjem/65pcZKuHDqSlS76Q90Qw2U/973124efa30fd55090c7d2d65355af78/1_2x.jpg?w=450&h=450">
+                    </div>
+                    <h3>Wasdroger <?php echo "" . $row ?></h3>
+                    <p>
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent at
+                        tincidunt justo. Nam rutrum diam quis ante ornare, non dictum sem
+                        tempus...
+                    </p>
+                    <h4 class="col-md-6 text-warning p-0">
+                        <?php
+                        for ($i = 0; $i < 5; $i++) {
+                            if ($rating >= 1) {
+                        ?><span class="fas fa-star"></span>
+                            <?php
+                            } else if ($rating == 0.5) {
+                            ?><span class="fas fa-star-half-alt"></span>
+                            <?php
+                            } else {
+                            ?><span class="far fa-star"></span>
+                        <?php
+                            }
+                            $rating -= 1;
+                        }
+                        ?>
+                    </h4>
+                    <h4 class="col-md-6">
+                        Prijs: €200,-
+                    </h4>
+                    <button type="button" class="btn btn-primary btn-block">
+                        <i class="fas fa-plus"></i> Voeg toe aan winkelwagen
+                    </button>
+                </div>
+                <?php
+                if ($rowCount % $numOfCols == 0) { ?>
+                </div> <?php }
+                } ?>
+    </div>
+    <div class="col-md-1">
+        <p>WINKELMAND</p>
+    </div>
+    <?php
+
+    include __DIR__ . '/../footer.php';
+    ?>
