704e366ba24ac9af051e3a63d9ecef5ac4349153
Job <jobengelen@yahoo.nl>
2022-01-05 20:14:59 +0100

    Postgresql initial.sql added

--- app/dbconfig.php
+++ app/dbconfig.php
@@ -1,6 +1,14 @@
 <?php
-$servername = "mysql";
-$username = "root";
-$password = "secret123";
-$database = "helpmijnwasisnat";
+/*$servername = "ec2-54-170-163-224.eu-west-1.compute.amazonaws.com";
+$username = "hrqsdzmgbuffkf";
+$password = "66c2887876ab2a5d4e11030d355e50e0fd5e35830715299a7fa8cfad60d66bfb";
+$database = "helpmijnwasisnat";*/
+
+
+
+$host = "ec2-54-170-163-224.eu-west-1.compute.amazonaws.com";
+$port = 5432;
+$dbname = "d9db5rg712gld5";
+$user = "hrqsdzmgbuffkf";
+$password = "66c2887876ab2a5d4e11030d355e50e0fd5e35830715299a7fa8cfad60d66bfb";
 ?>
--- app/repositories/repository.php
+++ app/repositories/repository.php
@@ -3,7 +3,7 @@
 class Repository
 {
 
-    protected $connection;
+    /*protected $connection;
 
     function __construct()
     {
@@ -15,6 +15,28 @@ class Repository
             $this->connection->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
         } catch (PDOException $e) {
             echo "Connection failed: " . $e->getMessage();
+        }
+    }*/
+
+    protected $pdo;
+
+
+    function __construct()
+    {
+
+        require __DIR__ . '/../dbconfig.php';
+        try {
+            $dsn = "pgsql:host=$host;port=$port;dbname=$db;";
+            // make a database connection
+            $pdo = new PDO($dsn, $user, $password, [PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION]);
+            // set the PDO error mode to exception
+            $this->connection->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        } catch (PDOException $e) {
+            die($e->getMessage());
+        } finally {
+            if ($pdo) {
+                $pdo = null;
+            }
         }
     }
 }
